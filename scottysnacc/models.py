705d8b8d0cc85c49650b8741a86259a06e7c5edc
Vicky Liu <91029793+itsvickyliu@users.noreply.github.com>
2023-03-21 16:34:40 -0400

    Created Profile and Event models

--- scottysnacc/models.py
+++ scottysnacc/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Event(models.Model):
+    user = models.ForeignKey(User, default=None, on_delete=models.PROTECT)
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    # location = models.PointField(geography=True, spatial_index=True)
+    date = models.DateTimeField()
+    tag = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'id={self.id}, user={self.user}, name={self.name}'
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User, default=None, on_delete=models.PROTECT)
+    picture = models.FileField(blank=True)
+    liked_events  = models.ManyToManyField(Event)
+
+    def __str__(self):
+        return f'id={self.id}, user={self.user}'
+    
+
