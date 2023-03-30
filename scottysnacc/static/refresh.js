f47f4b2c99296b52b53da07244ba3bd2a5c84102
Erin Yan <68751020+Yan-Erin@users.noreply.github.com>
2023-03-30 16:42:59 -0400

    map closes new ebvemt

--- scottysnacc/static/refresh.js
+++ scottysnacc/static/refresh.js
@@ -137,6 +137,24 @@ function deleteEvent(event_id) {
     xhr.send(`csrfmiddlewaretoken=${getCSRFToken()}`)
 }
 
+// context = {"eventNameValue": eventNameValue,
+//"buildingValue": buildingValue,
+//"specLocationValue": specLocationValue,
+//"startTimeValue": startTimeValue,
+//"endTimeValue": endTimeElement
+//}
+function checkEventForm (context) {
+    if (context["eventNameValue"] == "" || context["buildingValue"] == '' || context["specLocationValue"] == "") {
+        console.log("1")
+        return false;
+    }
+    if (context["startTimeValue"] == "" || context["endTimeValue"]== ""){
+        console.log("2")
+        return false;
+    }
+    return true;
+}
+
 function addEvent() {
     let eventNameElement = document.getElementById("id_name_input_text")
     let eventNameValue = eventNameElement.value
@@ -163,6 +181,19 @@ function addEvent() {
     let tagElement = document.getElementById("id_tag_input_text")
     let tagValue = tagElement.value
 
+    var context = {"eventNameValue": eventNameValue,
+               "buildingValue": buildingValue,
+               "specLocationValue": specLocationValue,
+               "startTimeValue": startTimeValue,
+               "endTimeValue": endTimeElement
+            }
+    if (checkEventForm(context)) {
+        console.log("WORKED");
+        var x = document.getElementById("new-event-block");
+        x.style.display = "none";
+        var btn = document.getElementById("new-event-btn")
+        btn.style.display = "block";
+    }
     eventNameElement.value = ''
     buildingElement.value = ''
     descriptionElement.value = ''
@@ -209,32 +240,49 @@ function makeNewEventBlock() {
         <div id="new-event-block">
             <div id="new-event-text">
             <div class='new-event-img'>
+                <button class="close-btn" onclick="closeEvent()">x</button>
                 <img  src="static/img/grid_small_round.svg" >
                 <h2>Add an Event</h2>
                 <p> Fill out form to enter your event </p>
             </div>
             
             <div class='form-group event-form'>
-                <button class="close-btn" onclick="closeEvent()">x</button>
-                <label>Event Name</label>
+                <label><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-map" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M15.817.113A.5.5 0 0 1 16 .5v14a.5.5 0 0 1-.402.49l-5 1a.502.502 0 0 1-.196 0L5.5 15.01l-4.902.98A.5.5 0 0 1 0 15.5v-14a.5.5 0 0 1 .402-.49l5-1a.5.5 0 0 1 .196 0L10.5.99l4.902-.98a.5.5 0 0 1 .415.103zM10 1.91l-4-.8v12.98l4 .8V1.91zm1 12.98 4-.8V1.11l-4 .8v12.98zm-6-.8V1.11l-4 .8v12.98l4-.8z"/>
+                </svg>   Event Name</label>
                 <input type="text" name="name"  class="form-control" id="id_name_input_text"/>
-
-                <label>Building Location</label>
+                <label><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 16 16">
+                <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z"/>
+                <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                </svg>  Building Location</label>
                 <input type="text" name="building_location" class="form-control" id="id_building_location_input_text"/>
 
-                <label>Floor/Room Location</label>
+                <label><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M8 1a3 3 0 1 0 0 6 3 3 0 0 0 0-6zM4 4a4 4 0 1 1 4.5 3.969V13.5a.5.5 0 0 1-1 0V7.97A4 4 0 0 1 4 3.999zm2.493 8.574a.5.5 0 0 1-.411.575c-.712.118-1.28.295-1.655.493a1.319 1.319 0 0 0-.37.265.301.301 0 0 0-.057.09V14l.002.008a.147.147 0 0 0 .016.033.617.617 0 0 0 .145.15c.165.13.435.27.813.395.751.25 1.82.414 3.024.414s2.273-.163 3.024-.414c.378-.126.648-.265.813-.395a.619.619 0 0 0 .146-.15.148.148 0 0 0 .015-.033L12 14v-.004a.301.301 0 0 0-.057-.09 1.318 1.318 0 0 0-.37-.264c-.376-.198-.943-.375-1.655-.493a.5.5 0 1 1 .164-.986c.77.127 1.452.328 1.957.594C12.5 13 13 13.4 13 14c0 .426-.26.752-.544.977-.29.228-.68.413-1.116.558-.878.293-2.059.465-3.34.465-1.281 0-2.462-.172-3.34-.465-.436-.145-.826-.33-1.116-.558C3.26 14.752 3 14.426 3 14c0-.599.5-1 .961-1.243.505-.266 1.187-.467 1.957-.594a.5.5 0 0 1 .575.411z"/>
+                </svg>  Floor/Room Location</label>
                 <input type="text" name="specific_location" class="form-control" id="id_specific_location_input_text"/>
 
-                <label>Start Time</label>
+                <label><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clock" viewBox="0 0 16 16">
+                <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"/>
+                <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z"/>
+                </svg>  Start Time</label>
                 <input type="text" name="start_time" class="form-control" id="id_start_time_input_text"/>
 
-                <label>End Time</label>
+                <label><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clock-fill" viewBox="0 0 16 16">
+                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"/>
+                </svg>  End Time</label>
                 <input type="text" name="end_time" class="form-control" id="id_end_time_input_text"/>
 
-                <label>Description</label>
+                <label><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-newspaper" viewBox="0 0 16 16">
+                <path d="M0 2.5A1.5 1.5 0 0 1 1.5 1h11A1.5 1.5 0 0 1 14 2.5v10.528c0 .3-.05.654-.238.972h.738a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 1 1 0v9a1.5 1.5 0 0 1-1.5 1.5H1.497A1.497 1.497 0 0 1 0 13.5v-11zM12 14c.37 0 .654-.211.853-.441.092-.106.147-.279.147-.531V2.5a.5.5 0 0 0-.5-.5h-11a.5.5 0 0 0-.5.5v11c0 .278.223.5.497.5H12z"/>
+                <path d="M2 3h10v2H2V3zm0 3h4v3H2V6zm0 4h4v1H2v-1zm0 2h4v1H2v-1zm5-6h2v1H7V6zm3 0h2v1h-2V6zM7 8h2v1H7V8zm3 0h2v1h-2V8zm-3 2h2v1H7v-1zm3 0h2v1h-2v-1zm-3 2h2v1H7v-1zm3 0h2v1h-2v-1z"/>
+                </svg>  Description</label>
                 <input type="text" name="description" class="form-control" id="id_description_input_text"/>
 
-                <label>Tags</label>
+                <label><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-tag" viewBox="0 0 16 16">
+                <path d="M6 4.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm-1 0a.5.5 0 1 0-1 0 .5.5 0 0 0 1 0z"/>
+                <path d="M2 1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 1 6.586V2a1 1 0 0 1 1-1zm0 5.586 7 7L13.586 9l-7-7H2v4.586z"/>
+                </svg>  Tags</label>
                 <input type="text" name="tags" class="form-control" id="id_tag_input_text"/>
 
                 <button  id='event-submit' class='btn-default' onclick="addEvent()">Submit</button>
