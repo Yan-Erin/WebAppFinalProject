8527d335ad83ded1b4a0501e3ceccc158064f01c
Erin Yan <68751020+Yan-Erin@users.noreply.github.com>
2023-04-12 14:24:36 -0400

    error message wrong oauth

--- scottysnacc/custom_pipeline.py
+++ scottysnacc/custom_pipeline.py
@@ -1,11 +1,12 @@
 from social_core.exceptions import AuthForbidden
 from django.shortcuts import redirect
 from django.urls import reverse
+from django.contrib import messages
 
 def check_email_domain(backend, details, response, *args, **kwargs):
     email = details.get('email', '')
-    print(email)
     if email and email.endswith('@andrew.cmu.edu'):
         return
     else:
+        messages.error(kwargs['request'], "Invalid email domain. Please use an '@andrew.cmu.edu' email.")
         return redirect(reverse('login'))
